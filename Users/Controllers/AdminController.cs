f50351f28098cfe74a787f2c3a035a34bffd9617
TaniaTru <tatiana.trukhachova@gmail.com>
2018-06-08 15:10:12 +0300

    Create action and View

--- Users/Controllers/AdminController.cs
+++ Users/Controllers/AdminController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
 using Users.Infrastructure;
+using Users.Models;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
 
 namespace Users.Controllers
 {
@@ -19,6 +22,38 @@ namespace Users.Controllers
             get
             {
                 return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser { UserName = model.Name, Email = model.Email };
+                IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
             }
         }
     }
