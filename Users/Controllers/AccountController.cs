3b4d3c3dab00d7f762ed76e9e319ece08d57f3ac
TaniaTru <tatiana.trukhachova@gmail.com>
2018-06-09 19:06:52 +0300

    Account

---
+++ Users/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+[AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel details, string returnUrl)
+        {
+            return View(details);
+        }
+    }
+}
+// When a user
+//requests a restricted URL, they are redirected to the /Account/Login URL with a query string that specifies the URL
+//that the user should be sent back to once they have been authenticated.
