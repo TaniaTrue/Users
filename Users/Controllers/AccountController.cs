8303ed1ead21fb5a5b0976cee553ea512ab27197
TaniaTru <tatiana.trukhachova@gmail.com>
2018-06-09 19:33:01 +0300

    Login action

--- Users/Controllers/AccountController.cs
+++ Users/Controllers/AccountController.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Users.Models;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Users.Infrastructure;
 
 namespace Users.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
-[AllowAnonymous]
+        [AllowAnonymous]
         public ActionResult Login(string returnUrl)
         {
-            if (ModelState.IsValid)
-            {
-            }
             ViewBag.ReturnUrl = returnUrl;
             return View();
         }
@@ -26,7 +26,21 @@ namespace Users.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(LoginModel details, string returnUrl)
         {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await UserManager.FindAsync(details.Name,
+   details.Password);
+
+            }
             return View(details);
+        }
+
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
         }
     }
 }
